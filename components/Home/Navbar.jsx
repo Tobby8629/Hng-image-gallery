bed843eb66e500536aff016a4297b427006236e6
Samuel tobby <sarhmullah15@gmail.com>
2023-09-21 09:33:07 -0700

    update the code

--- components/Home/Navbar.jsx
+++ components/Home/Navbar.jsx
@@ -3,9 +3,9 @@ import { signOut } from "next-auth/react";
 
 const NavBar = () => {
     return (
-     <section className="flex justify-between md:p-5 lg:p-7">
-       <h2>Image Gallery</h2>
-       <button onClick={()=>signOut()}>Logout</button>
+     <section className="flex justify-between p-2 px-3 md:p-5 lg:p-7 fixed top-0 w-full z-40 bg-black xl:px-10">
+       <h2 className="border-1 border-rose-600 curve text-xl md:text-3xl md:p-2 md:px-3 lg:text-5xl lg:px-5 px-2">Image Gallery</h2>
+       <button onClick={()=>signOut()} className="lg:text-3xl poppins">Logout</button>
      </section>   
     );
 }
